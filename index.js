793958ae0d87a5f7821a8835999319357cb35c64
Onliy <91193667+ImOnple@users.noreply.github.com>
2024-06-18 19:45:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ myAudio.src = 'MOGOVOLONIO.mp3';
 
 function audioo() {
     myAudio.play();
+    myAudio.loop=true;
     document.querySelector('.button').remove();
     document.querySelector('.text').remove();
 }
